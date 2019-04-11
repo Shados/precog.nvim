5613503197a0d6f95c3b6d2cf4fc56d8786c1c3e
Alexei Robyn <shados@shados.net>
2019-04-11 21:55:23 +1000

    Fix some bugs in path parsing

--- lua/precog/path.lua
+++ lua/precog/path.lua
@@ -1,9 +1,7 @@
 require('precog')
 require('earthshine.string')
 local vimw = require('facade')
-local utils = require('precog.utils')
-local p = require('earthshine.path')
-local inspect = require('vendor.inspect')
+local Path = require('earthshine.path')
 local map_raw_to_escaped, map_escaped_to_raw
 precog.path = {
   source_name = "path",
@@ -23,116 +21,52 @@ precog.path = {
     local change_data = precog.change_data[timer_id]
     local line_content, before_cursor, start_col, col, filepath
     line_content, before_cursor, start_col, col, filepath = change_data.line_content, change_data.before_cursor, change_data.start_col, change_data.col, change_data.filepath
-    local path, quoted
-    local after_cursor = line_content:at(col)
-    if self.valid_quotes[after_cursor] then
-      local quote = after_cursor
-      path = self:find_inner_path(before_cursor, quote)
+    local maybe_path = before_cursor:match("[^%z]+$")
+    if not (maybe_path) then
+      return 
     end
-    if not (path) then
-      path = self:find_unquoted_path(before_cursor)
-      quoted = false
-    else
-      quoted = true
+    local first, quoted = self:find_path_bounds(maybe_path)
+    if not (first ~= nil) then
+      return 
     end
-    local parent_dir_path, file_prefix, ends_with_dir = self:get_path_properties(path, filepath)
-    return self:schedule_ls_job(timer_id, parent_dir_path, file_prefix, ends_with_dir, not quoted)
-  end,
-  get_path_properties = function(self, path, filepath)
-    local dir_path
-    if p.is_absolute(path) then
-      dir_path = "/"
-    else
-      dir_path = p.parse_dir(filepath)
-    end
-    if (dir_path:at(#dir_path)) ~= "/" then
-      dir_path = dir_path .. "/"
-    end
-    local path_elements = { }
-    for element in p.iterate(path) do
-      table.insert(path_elements, element)
-    end
-    local ends_with_dir = #path == 0 or (path:at(#path)) == '/'
-    local file_prefix
-    if not (ends_with_dir) then
-      file_prefix = path_elements[#path_elements]
-    else
-      file_prefix = nil
-    end
-    local loop_end
-    if ends_with_dir then
-      loop_end = #path_elements
-    else
-      loop_end = #path_elements - 1
-    end
-    for i = 1, loop_end do
-      local element = path_elements[i]
-      dir_path = dir_path .. (element .. "/")
-    end
-    return dir_path, file_prefix, ends_with_dir
-  end,
-  filter_children = function(self, children, file_prefix, ends_with_dir, should_escape)
-    local matches = { }
-    for _index_0 = 1, #children do
-      local _continue_0 = false
-      repeat
-        local filename = children[_index_0]
-        if filename == "." or filename == ".." or filename == "" then
-          _continue_0 = true
-          break
-        end
-        if should_escape then
-          filename = self:escape_filename(filename)
-        end
-        if ends_with_dir then
-          table.insert(matches, filename)
-        elseif (filename:sub(1, #file_prefix)) == file_prefix then
-          table.insert(matches, filename)
-        end
-        _continue_0 = true
-      until true
-      if not _continue_0 then
-        break
-      end
-    end
-    return matches
-  end,
-  path_pattern = "([^%z]*)",
-  find_inner_path = function(self, str, quote)
-    local last_escape, last_quote
-    for index = 1, #str do
-      local char = str:at(index)
-      if char == "\\" then
-        last_escape = index
-      elseif char == quote and last_escape ~= index - 1 then
-        last_quote = index
-      end
-    end
-    local path_start
-    if last_quote then
-      path_start = last_quote + 1
-    else
-      path_start = nil
-    end
-    if path_start then
-      local inner_string = str:sub(path_start, #str)
-      if inner_string:match(self.path_pattern) then
-        return inner_string
-      end
-    end
-    return nil
-  end,
-  find_unquoted_path = function(self, str)
-    str = utils.get_last_match(self.path_pattern, str)
-    if str then
-      return str:gsub("\\.", function(s)
+    local path = maybe_path:sub(first, -1)
+    if not (quoted) then
+      path = path:gsub("\\.", function(s)
         return self:unescape_chars(s)
       end)
+    end
+    local ends_with_dir = (path:at(#path)) == "/"
+    local ls_path, file_prefix
+    if ends_with_dir then
+      ls_path = path
+      file_prefix = nil
     else
-      return ""
+      ls_path = Path:parse_parent(path)
+      file_prefix = Path:parse_name(path)
+    end
+    return self:schedule_ls_job(timer_id, ls_path, file_prefix, (not quoted))
+  end,
+  find_path_bounds = function(self, path)
+    local first, last, quoted = 0, #path, false
+    local last_char, char
+    for index = 1, #path do
+      char = path:at(index)
+      if self.valid_quotes[char] and last_char ~= "\\" then
+        quoted = not quoted
+        first = index + 1
+      end
+    end
+    if first > last then
+      return nil, true
+    else
+      return first, quoted
     end
   end,
-  schedule_ls_job = function(self, predictor_id, path, file_prefix, ends_with_dir, should_escape)
+  schedule_ls_job = function(self, predictor_id, path, file_prefix, should_escape)
+    local path_prefix
+    if path == "." then
+      path_prefix = "/"
+    end
     return vimw.fn("jobstart", {
       {
         'ls',
@@ -145,14 +79,14 @@ precog.path = {
         stdout_buffered = true,
         predictor_id = predictor_id,
         file_prefix = file_prefix,
-        ends_with_dir = ends_with_dir,
-        should_escape = should_escape
+        should_escape = should_escape,
+        path_prefix = path_prefix
       }
     })
   end,
   ls_callback = function(self, job_id, opts, data)
-    local predictor_id, file_prefix, ends_with_dir, should_escape
-    predictor_id, file_prefix, ends_with_dir, should_escape = opts.predictor_id, opts.file_prefix, opts.ends_with_dir, opts.should_escape
+    local predictor_id, file_prefix, should_escape, path_prefix
+    predictor_id, file_prefix, should_escape, path_prefix = opts.predictor_id, opts.file_prefix, opts.should_escape, opts.path_prefix
     local children = { }
     for _index_0 = 1, #data do
       local line = data[_index_0]
@@ -160,13 +94,55 @@ precog.path = {
         return self:unescape_chars(s)
       end)))
     end
-    local candidates = self:filter_children(children, file_prefix, ends_with_dir, should_escape)
+    local candidates = self:filter_children(children, file_prefix, should_escape, path_prefix)
     return precog:add_candidates(self.source_name, predictor_id, candidates)
+  end,
+  filter_children = function(self, children, file_prefix, should_escape, path_prefix)
+    local matches = { }
+    for _index_0 = 1, #children do
+      local _continue_0 = false
+      repeat
+        local filename = children[_index_0]
+        if filename == "." or filename == ".." or filename == "" then
+          _continue_0 = true
+          break
+        end
+        if should_escape then
+          filename = self:escape_filename(filename)
+        else
+          filename = self:escape_escapes(filename)
+        end
+        if path_prefix then
+          filename = path_prefix .. filename
+        end
+        if not file_prefix then
+          table.insert(matches, filename)
+        elseif (filename:sub(1, #file_prefix)) == file_prefix then
+          table.insert(matches, filename)
+        end
+        _continue_0 = true
+      until true
+      if not _continue_0 then
+        break
+      end
+    end
+    return matches
   end,
   escape_filename = function(self, filename)
     local escaped_filename = ""
     for char in filename:chars() do
       escaped_filename = escaped_filename .. self:escape_char(char)
+    end
+    return escaped_filename
+  end,
+  escape_escapes = function(self, filename)
+    local escaped_filename = ""
+    for char in filename:chars() do
+      if char == "\\" then
+        escaped_filename = escaped_filename .. self:escape_char(char)
+      else
+        escaped_filename = escaped_filename .. char
+      end
     end
     return escaped_filename
   end,
@@ -195,3 +171,4 @@ do
   end
   map_escaped_to_raw = _tbl_0
 end
+return precog.path
--- moon/precog/path.moon
+++ moon/precog/path.moon
@@ -2,9 +2,7 @@ export precog
 require 'precog'
 require 'earthshine.string'
 vimw = require 'facade'
-utils = require 'precog.utils'
-p = require 'earthshine.path'
-inspect = require 'vendor.inspect'
+Path = require 'earthshine.path'
 
 local *
 
@@ -25,127 +23,67 @@ precog.path =
     change_data = precog.change_data[timer_id]
     import line_content, before_cursor, start_col, col, filepath from change_data
 
-    -- Scan the line up to the current cursor position, looking for valid POSIX
-    -- paths.
-    -- Two code-paths, based on if the character *after* the cursor is a
-    -- quotation mark (', ", or `). If it is:
-    -- - We assume the path is quoted, look for a matching start quote, and
-    --   then look for a valid path within that.
-    -- - If not, or if there turns out to be no matching start quote, we assume
-    --   explicit escaping of spaces in path (using a \).
-    local path, quoted
+    -- Scan the portion of the line leading to the current cursor position,
+    -- looking for valid POSIX paths that end at the curpos.
+    -- We also need to work out whether the path up to the curpos is:
+    -- a) Within a valid pair of quotes, or after an opening quote
+    -- b) Unquoted
 
-    after_cursor = line_content\at col
+    -- The longest non-empty string of non-NUL bytes leading up to the curpos
+    maybe_path = before_cursor\match "[^%z]+$"
 
-    if @valid_quotes[after_cursor]
-      quote = after_cursor
-      path = @find_inner_path before_cursor, quote
+    return unless maybe_path
+    first, quoted = @find_path_bounds maybe_path
+    return unless first != nil
+    path = maybe_path\sub first, -1
+    unless quoted
+      -- Unescape any escaped characters
+      path = path\gsub "\\.", (s) -> @unescape_chars s
+      -- If not quoted, we assume explicit escaping of spaces and some other
+      -- characters in the path (using \ as the escape character).
 
-    unless path
-      path = @find_unquoted_path before_cursor
-      quoted = false
+    ends_with_dir = (path\at #path) == "/"
+    local ls_path, file_prefix
+    if ends_with_dir
+      ls_path = path
+      file_prefix = nil
     else
-      quoted = true
+      ls_path = Path\parse_parent path
+      file_prefix = Path\parse_name path
 
-    parent_dir_path, file_prefix, ends_with_dir = @get_path_properties path, filepath
-    @schedule_ls_job timer_id, parent_dir_path, file_prefix, ends_with_dir, not quoted
+    @schedule_ls_job timer_id, ls_path, file_prefix, (not quoted)
 
-  get_path_properties: (path, filepath) =>
-    dir_path = if p.is_absolute path
-      "/"
+  find_path_bounds: (path) =>
+    first, last, quoted = 0, #path, false
+    local last_char, char
+    for index = 1, #path
+      char = path\at index
+      if @valid_quotes[char] and last_char != "\\"
+        quoted = not quoted
+        first = index + 1
+    if first > last
+      -- Should only happen with a path like: /some/path/"
+      -- With curpos at the very end
+      return nil, true
     else
-      p.parse_dir filepath
-    if (dir_path\at #dir_path) != "/"
-      dir_path ..= "/"
+      return first, quoted
 
-    path_elements = {}
-    for element in p.iterate path
-      table.insert path_elements, element
-
-    ends_with_dir = #path == 0 or (path\at #path) == '/'
-    file_prefix = unless ends_with_dir
-      path_elements[#path_elements]
-    else
-      nil
-
-    -- If we have directory elements, add them to the dir_path
-    loop_end = if ends_with_dir
-      #path_elements
-    else
-      #path_elements - 1
-    for i = 1, loop_end
-      element = path_elements[i]
-      dir_path ..= element .. "/"
-
-    return dir_path, file_prefix, ends_with_dir
-
-  filter_children: (children, file_prefix, ends_with_dir, should_escape) =>
-    matches = {}
-    for filename in *children
-      continue if filename == "." or filename == ".." or filename == ""
-      if should_escape
-        filename = @escape_filename filename
-      if ends_with_dir
-        table.insert matches, filename
-      elseif (filename\sub 1, #file_prefix) == file_prefix
-        table.insert matches, filename
-    return matches
-
-  -- A valid unix path is 1 or more non-NUL characters. Really. In this case
-  -- we're also looking for potential paths, so 0 or more.
-  path_pattern: "([^%z]*)"
-
-  -- Returns: path within quoted string str (excluding closing quote), or nil
-  find_inner_path: (str, quote) =>
-    -- Scan string, look for and store the index of the last unescaped
-    -- matching quote
-    local last_escape, last_quote
-    for index = 1, #str
-      char = str\at index
-      if char == "\\"
-        last_escape = index
-      elseif char == quote and last_escape != index - 1
-        last_quote = index
-
-    path_start = if last_quote
-      -- Advance index by 1 to reach the first character within the quoted
-      -- string
-      last_quote + 1
-    else
-      nil
-
-    if path_start
-      -- Take the segment within the quotes and determine if it is a valid
-      -- path
-      inner_string = str\sub path_start, #str
-      if inner_string\match @path_pattern
-        return inner_string
-    return nil
-
-  find_unquoted_path: (str) =>
-    -- Find the last sequence of non-NUL characters in the input string
-    str = utils.get_last_match @path_pattern, str
-    -- Unescape any escaped characters and return
-    if str
-      return str\gsub "\\.", (s) -> @unescape_chars s
-    else
-      return ""
-
-  schedule_ls_job: (predictor_id, path, file_prefix, ends_with_dir, should_escape) =>
+  schedule_ls_job: (predictor_id, path, file_prefix, should_escape) =>
+    path_prefix = "/" if path == "."
     vimw.fn "jobstart", { {'ls', '-1', '-b', path}, {
       on_stdout: "precog#sources#path#ls_callback"
       stdout_buffered: true
       :predictor_id
       :file_prefix
-      :ends_with_dir
       :should_escape
+      :path_prefix
     }}
 
   -- TODO figure out a way to handle overly-large directories more cleanly.
   -- Maybe look into using an unbuffered callback to add predictions
   -- incrementally?
   ls_callback: (job_id, opts, data) =>
-    import predictor_id, file_prefix, ends_with_dir, should_escape from opts
+    import predictor_id, file_prefix, should_escape, path_prefix from opts
 
     -- Unescape the returned child files
     children = {}
@@ -156,14 +94,40 @@ precog.path =
     -- Check the returned children of the given dir_path and see if any match
     -- using the last element in the typed path as a prefix (if the last
     -- element is a file)
-    candidates = @filter_children children, file_prefix, ends_with_dir, should_escape
+    candidates = @filter_children children, file_prefix, should_escape, path_prefix
 
     precog\add_candidates @source_name, predictor_id, candidates
+
+  filter_children: (children, file_prefix, should_escape, path_prefix) =>
+    matches = {}
+    for filename in *children
+      continue if filename == "." or filename == ".." or filename == ""
+      if should_escape
+        filename = @escape_filename filename
+      else
+        -- Still need to escape the escape character in quoted paths
+        filename = @escape_escapes filename
+      if path_prefix
+        filename = path_prefix .. filename
+      if not file_prefix
+        table.insert matches, filename
+      elseif (filename\sub 1, #file_prefix) == file_prefix
+        table.insert matches, filename
+    return matches
 
   escape_filename: (filename) =>
     escaped_filename = ""
     for char in filename\chars!
       escaped_filename ..= @escape_char char
+    return escaped_filename
+
+  escape_escapes: (filename) =>
+    escaped_filename = ""
+    for char in filename\chars!
+      if char == "\\"
+        escaped_filename ..= @escape_char char
+      else
+        escaped_filename ..= char
     return escaped_filename
 
   -- Returns a C-style escaped version of the input character
@@ -186,3 +150,5 @@ map_raw_to_escaped = {
   [" "]: "\\ "
 }
 map_escaped_to_raw = {escaped, raw for raw, escaped in pairs map_raw_to_escaped}
+
+return precog.path
